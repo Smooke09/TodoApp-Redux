0806f505d7b77a3acd28211f820759bb938b0716
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-22 12:00:33 -0300

    Application Middleware(Redux-thunk)

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -3,16 +3,17 @@ const INITIAL_STATE = {
   list: [],
 };
 
-export default (state = INITIAL_STATE, action) => {
+const todoReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "DESCRIPTION_CHANGED":
       return { ...state, description: action.payload };
     case "TODO_SEARCHED":
-      return {
-        ...state,
-        list: action.payload.data,
-      };
+      return { ...state, list: action.payload.data };
+    case "TODO_ADDED":
+      return { ...state, description: "" };
     default:
       return state;
   }
 };
+
+export default todoReducer;
